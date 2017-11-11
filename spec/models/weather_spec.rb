dbf67a1edc34a6482efbcfac67076fe956e82fb9
Rafael B. Tauil <rafael@tauil.com.br>
2017-11-11 17:25:24 -0200

    Removes old code

--- spec/models/weather_spec.rb
+++ spec/models/weather_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe Weather, type: :model do
   describe '.by_city_and_country' do
     let!(:params) { {city_name: 'Rio de Janeiro', country_name: 'Brazil'} }
-    #let!(:mocked_uri) { URI("#{described_class.api_endpoint}/data/2.5/weather?q=Rio%20de%20Janeiro,Brazil&appid=#{described_class.api_key}") }
-    let!(:mocked_uri) { URI("http://api.openweathermap.org/data/2.5/weather?q=Rio%20de%20Janeiro,Brazil&appid=8f6414ac7111cd12c652f83bd994dc3e") }
+    let!(:mocked_uri) { URI("#{described_class.api_endpoint}/data/2.5/weather?q=Rio%20de%20Janeiro,Brazil&appid=#{described_class.api_key}") }
     let!(:response_mock) { "{\"coord\":{\"lon\":-43.21,\"lat\":-22.9},\"weather\":[{\"id\":501,\"main\":\"Rain\",\"description\":\"moderate rain\",\"icon\":\"10d\"},{\"id\":701,\"main\":\"Mist\",\"description\":\"mist\",\"icon\":\"50d\"}],\"base\":\"stations\",\"main\":{\"temp\":295.15,\"pressure\":1007,\"humidity\":88,\"temp_min\":295.15,\"temp_max\":295.15},\"visibility\":1200,\"wind\":{\"speed\":2.1,\"deg\":250},\"clouds\":{\"all\":90},\"dt\":1510423200,\"sys\":{\"type\":1,\"id\":4446,\"message\":0.003,\"country\":\"BR\",\"sunrise\":1510387324,\"sunset\":1510434728},\"id\":3451190,\"name\":\"Rio de Janeiro\",\"cod\":200}" }
 
     subject { described_class.by_city_and_country(params) }
